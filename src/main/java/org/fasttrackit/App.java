308605b34e3af09c2a91cfd55f34b481d474752d
oana.tibil <oana.tibil@gmail.com>
2019-01-17 19:39:40 +0200

    studied arrays

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -10,71 +10,76 @@ import java.time.LocalDateTime;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        Engine engine = new Engine();
-        engine.capacity = 1500;
-        engine.manufacturer = "Renault";
-        engine.expiryDate = LocalDateTime.now().minusYears(10);
-
-        Car car = new Car(engine);
-        car.setName( "Dacia");
-        car.setColor ("white");
-        car.doorCount = 4;
-        car.setMileage (7.5);
-        car.running = true;
-        System.out.println(car.toString());
-//        System.out.println(car.getName());
+  public static void main( String[] args )
+ {
+//        Engine engine = new Engine();
+//        engine.capacity = 1500;
+//        engine.manufacturer = "Renault";
+//        engine.expiryDate = LocalDateTime.now().minusYears(10);
 //
-//        car.engine = new Engine(); // relatie de tip has-a //
+//        Car car = new Car(engine);
+//        car.setName( "Dacia");
+//        car.setColor ("white");
+//        car.doorCount = 4;
+//        car.setMileage (7.5);
+//        car.running = true;
+//        System.out.println(car.toString());
+////        System.out.println(car.getName());
+////
+////        car.engine = new Engine(); // relatie de tip has-a //
+////
+////        car.engine = engine; //atribuim engine la car//
+////
+////        Engine engine2 = new Engine();
+////        engine2.capacity = 1200;
+////        engine.manufacturer = "BMW";
+////        Car car2 = new Car(engine2);
+////
+////        engine = null;
+////        AutoVehicle auto1 = new AutoVehicle(engine2);
+////        //facem prima masina sa accelereze//
+////
+////        double carTraveledDistance
+////                = car.accelerate(100, 2);
 //
-//        car.engine = engine; //atribuim engine la car//
+//        //example of static variables unique for the whole application//
+//Vehicle vehicle1 = new Vehicle();
+//vehicle1.vehicleCount = 1;
 //
-//        Engine engine2 = new Engine();
-//        engine2.capacity = 1200;
-//        engine.manufacturer = "BMW";
-//        Car car2 = new Car(engine2);
+//Vehicle vehicle2 = new Vehicle();
+//vehicle2.vehicleCount = 2;
 //
-//        engine = null;
-//        AutoVehicle auto1 = new AutoVehicle(engine2);
-//        //facem prima masina sa accelereze//
+//Vehicle.vehicleCount = 3;
 //
-//        double carTraveledDistance
-//                = car.accelerate(100, 2);
+//        System.out.println("Value from vehicle 1 :" + vehicle1.vehicleCount);
+//        System.out.println("Value from vehicle 2 :" + vehicle2.vehicleCount);
+//        System.out.println("Value from vehicle class :" + Vehicle.vehicleCount);
+//
+//   new AutoVehicle(new Engine ());
+//
+//   vehicle1.accelerate (34.12);
+//    vehicle2.accelerate (34.12, 0.8);
+//    new UFO().accelerate (130, 0.8);
+//
+//
+//    //polymrphism (an object can take multiple forms)
+//    Vehicle ufo = new UFO();
+//
+//    //The type of the variable does not deterine the object's behaviour
+//    ufo.accelerate (200, 2);
+//
+//    //the type of the variable determines what methods can be invoked
+//        //We might need type casting (see below)
+//        ((UFO) ufo).concealCheating();
+//
+//    }
+//
 
-        //example of static variables unique for the whole application//
-Vehicle vehicle1 = new Vehicle();
-vehicle1.vehicleCount = 1;
+     Game game = new Game();
+        game.start();
 
-Vehicle vehicle2 = new Vehicle();
-vehicle2.vehicleCount = 2;
-
-Vehicle.vehicleCount = 3;
-
-        System.out.println("Value from vehicle 1 :" + vehicle1.vehicleCount);
-        System.out.println("Value from vehicle 2 :" + vehicle2.vehicleCount);
-        System.out.println("Value from vehicle class :" + Vehicle.vehicleCount);
-
-   new AutoVehicle(new Engine ());
-
-   vehicle1.accelerate (34.12);
-    vehicle2.accelerate (34.12, 0.8);
-    new UFO().accelerate (130, 0.8);
-
-
-    //polymrphism (an object can take multiple forms)
-    Vehicle ufo = new UFO();
-
-    //The type of the variable does not deterine the object's behaviour
-    ufo.accelerate (200, 2);
-
-    //the type of the variable determines what methods can be invoked
-        //We might need type casting (see below)
-        ((UFO) ufo).concealCheating();
 
     }
-
-
-    }
+}
 
 
--- src/main/java/org/fasttrackit/Game.java
+++ src/main/java/org/fasttrackit/Game.java
@@ -2,7 +2,38 @@ package org.fasttrackit;
 
 public class Game {
 
+    private Track[] tracks = new Track[10];
+
+    // ? int[] integers = new int[10];?
+
     Vehicle firstCompetitor;
     Vehicle secondCompetitor;
+
+    public void start() {
+
+        addTracks();
+        displayAvailableTracks();
+
+    }
+
+    private void addTracks()
+    {
+
+        Track track1 = new Track("highway", 300);
+        Track track2 = new Track("seaside", 100);
+
+        tracks[0] = track1;
+        tracks[1] = track2;
+    }
+
+    private void displayAvailableTracks() {
+
+        System.out.println("Available tracks:");
+        System.out.println(tracks[0].getName());
+        System.out.println(tracks[1].getName());
+
+    }
+
+
 
 }
